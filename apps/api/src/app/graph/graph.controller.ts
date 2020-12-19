e463a36cf0a96516156b8befe4036ba831acea19
Daniel Marin <danielarturomt@gmail.com>
2020-12-19 20:51:15 -0300

    feat: set up a create graph endpoint AB#37

--- apps/api/src/app/graph/graph.controller.ts
+++ apps/api/src/app/graph/graph.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { CreateGraphDto } from '@slippery/shared/utils/api-interface';
 import { GraphService } from './graph.service';
 
 @Controller('/graphs')
 export class GraphController {
   constructor(private graphService: GraphService) {}
+
+  @Post()
+  create(@Body() createGraphDto: CreateGraphDto) {
+    return this.graphService.create(createGraphDto);
+  }
 }
